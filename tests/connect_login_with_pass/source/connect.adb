7d554fbc882fea77b3bded1393d520c5e86a8a64
Jerrid Kimball <jkimball@nairobi.home.ada.cx>
2012-10-23 11:21:36 -0500

    Correct path in project file. Get connection status before checking it.

--- tests/connect_login_with_pass/source/connect.adb
+++ tests/connect_login_with_pass/source/connect.adb
@@ -23,6 +23,8 @@ begin
 
    Ada.Text_IO.Put_Line ("Connection Status => " & PGAda.Connections.Connection_Status'Image (PGAda.Connections.Status (Connection => Conn) ) );
 
+   Status := PGAda.Connections.Status (Connection => Conn);
+
    if Status = PGAda.Connections.Connection_Bad then
       Ada.Text_IO.Put_Line ("Error Message => " & PGAda.Connections.Error_Message (Connection => Conn) );
    end if;
